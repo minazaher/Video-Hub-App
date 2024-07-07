5ab9c2709cf980f4614293ea2b7da4ea6b72e575
minazaher <menazaher115@gmail.com>
2024-07-07 06:31:46 +0300

    password-prompt styling added

--- src/app/components/password-prompt/password-prompt.component.ts
+++ src/app/components/password-prompt/password-prompt.component.ts
@@ -1,17 +1,37 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {TranslateModule} from "@ngx-translate/core";
+import { PasswordModule } from 'primeng/password';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-password-prompt',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    FontAwesomeModule,
+    PasswordModule,
+    TranslateModule,
+    NgIf,
+    NgClass
   ],
   templateUrl: './password-prompt.component.html',
   styleUrl: './password-prompt.component.scss'
 })
 export class PasswordPromptComponent {
+
+
+  visible:boolean= false;
+  showPassword: boolean = false;
   @Output() passwordSubmitted = new EventEmitter<string>();
+
+
+  viewPassword(){
+    this.visible = !this.visible
+    this.showPassword =!this.showPassword;
+  }
 
   submitPassword(passwordForm: any): void {
     if (passwordForm.valid) {
